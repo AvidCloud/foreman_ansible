722efe590594b23557623f84a26da177b8407d9f
Gerrit Schwerthelm <gerrit.schwerthelm@avid.com>
2017-02-07 11:45:42 +0100

    Add role inheritance on host group level.

--- app/lib/actions/foreman_ansible/play_hostgroup_roles.rb
+++ app/lib/actions/foreman_ansible/play_hostgroup_roles.rb
@@ -12,7 +12,7 @@ module Actions
                options = {})
         proxy = find_hostgroup_and_proxy(hostgroup, proxy_selector)
         inventory_creator = ::ForemanAnsible::
-          InventoryCreator.new(hostgroup.hosts)
+          InventoryCreator.new(gather_nested_hosts(hostgroup))
         playbook_creator = ::ForemanAnsible::
           PlaybookCreator.new(hostgroup_ansible_roles(hostgroup))
         plan_delegated_action(proxy, ::ForemanAnsibleCore::Actions::RunPlaybook,
@@ -33,23 +33,31 @@ module Actions
 
       def hostgroup_ansible_roles(hostgroup)
         role_names = []
-        hostgroup.hostgroup_ansible_roles.each do |ansible_role|
-          role_names.append(ansible_role.ansible_role_name)
+        hostgroup.all_ansible_roles.each do |ansible_role|
+          role_names.append(ansible_role.name)
         end
         role_names
       end
 
-      def hostgroup_contains_hosts(hostgroup)
-        return unless hostgroup.hosts.empty?
-        raise ::Foreman::Exception.new(N_('host group is empty'))
+      def gather_nested_hosts(hostgroup)
+        hosts = hostgroup.hosts
+        hostgroup.children.each do |child|
+          hosts += gather_nested_hosts(child)
+        end
+        hosts
       end
 
       def find_hostgroup_and_proxy(hostgroup, proxy_selector)
-        hostgroup_contains_hosts(hostgroup)
-        proxy = proxy_selector.determine_proxy(hostgroup.hosts[0])
+        hosts = gather_nested_hosts(hostgroup)
+        if hosts.empty?
+          raise ::Foreman::Exception.new(N_('host group is empty'))
+        end
+
+        proxy = proxy_selector.determine_proxy(hosts[0])
         input[:hostgroup] = { :id => hostgroup.id,
                               :name => hostgroup.name,
-                              :proxy_used => proxy.try(:name) || :not_defined }
+                              :proxy_used => proxy.try(:name) ||
+                                             :not_defined }
         proxy
       end
     end
